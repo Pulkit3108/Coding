e55bb77e1c32f3a4b649cd3be2407ded2a2d9d09
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2023-12-30 18:10:48 +0300

    Update alien.cpp

--- LeetCode Premium/269 - Alien Dictionary/alien.cpp
+++ LeetCode Premium/269 - Alien Dictionary/alien.cpp
@@ -10,6 +10,16 @@
 // 4. There may be multiple valid order of letters, return the smallest in normal lexicographical order.
 // 5. The letters in one string are of the same rank by default and are sorted in Human dictionary order.
 
+// Input：["wrt","wrf","er","ett","rftt"]
+// Output："wertf"
+
+// Explanation：
+// from "wrt"and"wrf" ,we can get 't'<'f'
+// from "wrt"and"er" ,we can get 'w'<'e'
+// from "er"and"ett" ,we can get 'r'<'t'
+// from "ett"and"rftt" ,we can get 'e'<'r'
+// So return "wertf"
+
 class Solution {
 public:
     string alienOrder(vector<string> &words) {
