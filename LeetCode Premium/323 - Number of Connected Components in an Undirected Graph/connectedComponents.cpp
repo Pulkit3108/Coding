4057c84bcbc5fa74c5f07b83a0747ebea15b3abb
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2023-12-30 18:19:39 +0300

    Update connectedComponents.cpp

--- LeetCode Premium/323 - Number of Connected Components in an Undirected Graph/connectedComponents.cpp
+++ LeetCode Premium/323 - Number of Connected Components in an Undirected Graph/connectedComponents.cpp
@@ -1,3 +1,15 @@
+// Description
+// In this problem, there is an undirected graph with n nodes. 
+// There is also an edges array. Where edges[i] = [a, b] means that there is an edge between node a and node b in the graph.
+
+// You need to return the number of connected components in that graph.
+
+// Example
+// Input:
+// 3
+// [[0,1], [0,2]]
+// Output: 1
+
 class Solution {
 public:
     void dfs(int node, vector<vector<int>> &adj, vector<bool> &visisted) {
