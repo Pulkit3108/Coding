a95b72657017fb8d2f30e1e3302c588d13988c95
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2023-12-30 18:13:26 +0300

    Update wiggle.cpp

--- LeetCode Premium/280 - Wiggle Sort/wiggle.cpp
+++ LeetCode Premium/280 - Wiggle Sort/wiggle.cpp
@@ -1,3 +1,13 @@
+// Description
+// Given an unsorted array nums, reorder it in-place such that
+
+// nums[0] <= nums[1] >= nums[2] <= nums[3]....
+// There may have multiple answers for a question, you only need to consider one of the possibilities.
+
+// Input: [3, 5, 2, 1, 6, 4]
+// Output: [1, 6, 2, 5, 3, 4]
+// Explanation: This question may have multiple answers, and [2, 6, 1, 5, 3, 4] is also ok.
+
 class Solution {
 public:
     void wiggleSort(vector<int> &nums) {
