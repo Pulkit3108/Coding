0c9c3546f823de2cd5bb7c07df8e8050b3ce7204
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2025-04-21 15:26:39 +0530

    Update meeting.cpp

--- LeetCode Premium/252 - Meeting Rooms/meeting.cpp
+++ LeetCode Premium/252 - Meeting Rooms/meeting.cpp
@@ -1,3 +1,25 @@
+// Problem Statement
+// Given an array of meeting time intervals consisting of start and end times [(s1, e1), (s2, e2), ...] (si < ei), 
+// determine if a person could attend all meetings.
+
+// Constraints:
+// 0 ≤ intervals.length ≤ 10^4
+// intervals[i].length == 2
+// 0 ≤ starti < endi ≤ 10^6
+// [(0,8), (8,10)] is not a conflict at 8.
+
+// Examples:
+
+// Example 1:
+// Input: intervals = [(0,30),(5,10),(15,20)]
+// Output: false
+// Explanation: (0,30), (5,10) and (0,30), (15,20) will conflict.
+
+// Example 2:
+// Input: intervals = [(5,8),(9,15)]
+// Output: true
+// Explanation: Two times will not conflict.
+
 /**
  * Definition of Interval:
  * class Interval {
@@ -16,19 +38,13 @@ public:
      * @param intervals: an array of meeting time intervals
      * @return: if a person could attend all meetings
      */
-    static bool compareInterval(const Interval &i1,const Interval &i2){
-        return i1.start < i2.start;
-    }
     bool canAttendMeetings(vector<Interval> &intervals) {
-        if(intervals.size()==0)
-            return true;
-        sort(intervals.begin(),intervals.end(),compareInterval);
-        Interval previousInterval=intervals[0];
-        for(int i=1;i<intervals.size();++i){
-            Interval currentInterval=intervals[i];
-            if(currentInterval.start<previousInterval.end)
-                return false;
-            previousInterval=currentInterval;
+        if (intervals.empty()) return true;
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
+            return a.start < b.start;
+        });
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start < intervals[i - 1].end) return false;
         }
         return true;
     }
