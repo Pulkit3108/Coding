5db8041501819b6d20d58fa3bcfa52481856dcde
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2025-04-21 15:29:14 +0530

    Update meeting2.cpp

--- LeetCode Premium/253 - Meeting Rooms II/meeting2.cpp
+++ LeetCode Premium/253 - Meeting Rooms II/meeting2.cpp
@@ -1,3 +1,25 @@
+// Problem Statement
+// Given an array of meeting time intervals consisting of start and end times [[s1, e1], [s2, e2], ...] (si < ei), 
+// find the minimum number of conference rooms required.
+
+// Constraints:
+// (0,8), (8,10) is not a conflict at 8.
+
+// Examples:
+
+// Example 1:
+// Input: intervals = [(0,30),(5,10),(15,20)]
+// Output: 2
+// Explanation: 
+// We need two meeting rooms:
+// room1: (0,30)
+// room2: (5,10), (15,20)
+
+// Example 2:
+// Input: intervals = [(2,7)]
+// Output: 1
+// Explanation: Only need one meeting room.
+
 /**
  * Definition of Interval:
  * class Interval {
@@ -16,27 +38,23 @@ public:
      * @param intervals: an array of meeting time intervals
      * @return: the minimum number of conference rooms required
      */
-
     int minMeetingRooms(vector<Interval> &intervals) {
-        if(intervals.empty())
-            return 0;
+        if (intervals.empty()) return 0;
 
-        sort(intervals.begin(), intervals.end(), [](const Interval &i1, const Interval &i2) {
-            return i1.start < i2.start;
+        sort(intervals.begin(), intervals.end(), [](const Interval &a, const Interval &b) {
+            return a.start < b.start;
         });
 
-        priority_queue<int, vector<int>, greater<int>> minHeap;
-        minHeap.push(intervals[0].end);
+        priority_queue<int, vector<int>, greater<int>> endTimes;
+        endTimes.push(intervals[0].end);
 
-        for(int i = 1; i < intervals.size(); i++) {
-
-            if(intervals[i].start >= minHeap.top()) {
-                minHeap.pop();
+        for (int i = 1; i < intervals.size(); ++i) {
+            if (intervals[i].start >= endTimes.top()) {
+                endTimes.pop();
             }
-
-            minHeap.push(intervals[i].end);
+            endTimes.push(intervals[i].end);
         }
 
-        return minHeap.size();
-    } 
+        return endTimes.size();
+    }
 };
