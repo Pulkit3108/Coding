6ffea0004368b2d15c7122f76eeadab69c7ce9ce
Pulkit Mittal <46241207+Pulkit3108@users.noreply.github.com>
2025-04-21 15:38:56 +0530

    Update wiggle.cpp

--- LeetCode Premium/280 - Wiggle Sort/wiggle.cpp
+++ LeetCode Premium/280 - Wiggle Sort/wiggle.cpp
@@ -1,22 +1,34 @@
-// Description
-// Given an unsorted array nums, reorder it in-place such that
+// Problem Statement
+// Given an unsorted array `nums`, reorder it in-place such that:
+// nums[0] <= nums[1] >= nums[2] <= nums[3]...
 
-// nums[0] <= nums[1] >= nums[2] <= nums[3]....
-// There may have multiple answers for a question, you only need to consider one of the possibilities.
+// Constraints:
+// - There may be multiple valid answers; you only need to consider one of the possibilities.
+// - Sort the array in place without defining additional arrays.
 
+// Examples:
+
+// Example 1:
 // Input: [3, 5, 2, 1, 6, 4]
 // Output: [1, 6, 2, 5, 3, 4]
-// Explanation: This question may have multiple answers, and [2, 6, 1, 5, 3, 4] is also ok.
+// Explanation: This question may have multiple answers, and [2, 6, 1, 5, 3, 4] is also valid.
+
+// Example 2:
+// Input: [1, 2, 3, 4]
+// Output: [1, 4, 2, 3]
 
 class Solution {
 public:
+    /**
+     * @param nums: A list of integers
+     * @return: nothing
+     */
     void wiggleSort(vector<int> &nums) {
-        int n=nums.size();
-        for(int i=1;i<n;++i){
-            if((i&1) && nums[i] < nums[i-1])
-                swap(nums[i-1],nums[i]);
-            if(!(i&1) && nums[i] > nums[i-1])
-                swap(nums[i-1],nums[i]);
+        for (int i = 1; i < nums.size(); ++i) {
+            if ((i % 2 == 1 && nums[i] < nums[i - 1]) || 
+                (i % 2 == 0 && nums[i] > nums[i - 1])) {
+                swap(nums[i], nums[i - 1]);
+            }
         }
     }
 };
